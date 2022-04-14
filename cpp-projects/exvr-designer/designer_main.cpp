76a24f86ca414bba29f2140800c14028513b57e3
Florian Lance <lance.florian@gmail.com>
2022-04-14 17:43:17 +0200

    New version

--- cpp-projects/exvr-designer/designer_main.cpp
+++ cpp-projects/exvr-designer/designer_main.cpp
@@ -37,7 +37,7 @@ using namespace tool::ex;
 int main(int argc, char *argv[]){
 
     // build parameters
-    const QString numVersion = "1.0a33";
+    const QString numVersion = "1.0a34";
     bool lncoComponents = true;
 
     // compiler check
