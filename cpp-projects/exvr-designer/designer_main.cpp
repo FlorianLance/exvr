e0442c9591f7721569e8edc7d15696045fd4dc6d
Florian Lance <lance.florian@gmail.com>
2022-05-31 17:27:39 +0200

    Update designer_main.cpp
    New version 1.0a45

--- cpp-projects/exvr-designer/designer_main.cpp
+++ cpp-projects/exvr-designer/designer_main.cpp
@@ -41,7 +41,7 @@ using namespace tool::ex;
 int main(int argc, char *argv[]){
 
     // build parameters
-    const QString numVersion = "1.0a44";
+    const QString numVersion = "1.0a45";
     bool lncoComponents = true;
 
     // compiler check
